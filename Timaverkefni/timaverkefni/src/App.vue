d3faf38facc8d5b0f17b6009e3567faf906526b4
CatarinaChainho <catarinachainhocosta@gmail.com>
2017-11-21 10:09:47 +0000

    list sorted

--- Timaverkefni/timaverkefni/src/App.vue
+++ Timaverkefni/timaverkefni/src/App.vue
@@ -1,25 +1,25 @@
 <template>
     <div id="app" class="column is-one-third is-offset-4">
-        <div class="column" style="margin:20px; padding-bottom: 20px;">
+        <div class="column" style="padding-bottom: 20px;">
           <label style="float:left;">Filter</label><br>
-          <div>
-            <input type="radio" name="all">All
-            <input type="radio" name="atlantsolia">Atlantsolia
-            <input type="radio" name="costco">CostCo Iceland
-            <input type="radio" name="daelan">Dælan
-            <input type="radio" name="n1">N1
-            <input type="radio" name="olis">Olís
-            <input type="radio" name="orkan">Orkan
-            <input type="radio" name="orkanx">Orkan x
-            <input type="radio" name="ob">ÓB
-          </div>
-          <p>Amount of Stations:{{}}</p>
+          <div style="text-align:center;">
+            <input type="radio" v-model="selectedCompanies" name="all">All
+            <input type="radio" v-model="selectedCompanies" name="atlantsolia">Atlantsolia
+            <input type="radio" v-model="selectedCompanies" name="costco">CostCo Iceland
+            <input type="radio" v-model="selectedCompanies" name="daelan">Dælan
+            <input type="radio" v-model="selectedCompanies" name="n1">N1 <br>
+            <input type="radio" v-model="selectedCompanies" name="olis">Olís
+            <input type="radio" v-model="selectedCompanies" name="orkan">Orkan
+            <input type="radio" v-model="selectedCompanies" name="orkanx">Orkan x
+            <input type="radio" v-model="selectedCompanies" name="ob">ÓB
+          </div><br>
         </div>
+          <p>Amount of Stations: {{response.length}}</p>
           <div class="box" id="place" v-for="petrol in response">
-              <h1 style="float:left;">{{petrol.company}}</h1>
-              <h1 style="float:right;">Bensin95: {{petrol.bensin95}}</h1><br>
-              <h1 style="float:left;">Station: {{petrol.name}}</h1>
-              <h1 style="float:right;">Diesel: {{petrol.diesel}}</h1>
+              <h1 style="float:left; color:black">{{petrol.company}}</h1>
+              <h1 style="float:right; color:gray">Bensin95: {{petrol.bensin95}}</h1><br>
+              <h1 style="float:left; color:black">Station: {{petrol.name}}</h1>
+              <h1 style="float:right; color:gray">Diesel: {{petrol.diesel}}</h1>
           </div>
     </div>
 </template>
@@ -30,24 +30,36 @@
           data() {
                return{
                     response: [],
-                    maxBensinPrice : 0
+                    maxBensinPrice : 0,
+                    selectedCompanies: "All"
                };
           },
-          computed: {
-              sortedStations(){
-                var self = this;
-                var sorted = this.response.sort(function(a,b){
-                  return parseFloat(a.bensin95) - parseFloat(b.bensin95);
-                });
+         computed: {
+               sortedStations(){
+                    var self = this;
+                    var sorted = this.response.sort(function(a,b){
+                         return parseFloat(a.bensin95) - parseFloat(b.bensin95);
+                     });
 
-                sorted =sorted.filter(function(response){
-                  return response.bensin95 < self.maxBensinPrice;
-                }.bind(this));  
-                return sorted;
-              }
+                    sorted =sorted.filter(function(response){
+                         return response.bensin95 < self.maxBensinPrice;
+                    }.bind(this));  
+                    return sorted;
 
-            },
-          mounted() {
+                    sorted =sorted.filter(function(response){
+                         if(this.selectedCompanies === "All"){var isSelected = true};
+                         return response.company === this.selectedCompanies || isSelected;
+                    }.bind(this));
+               },
+               filterCompanies(){
+                    var sortCompanies = this.response.sort();
+                    if(a.company < b.company) return -1;
+                    if(a.company > b.company) return 1;
+                    return 0;
+               }
+          },
+
+            mounted() {
                var self = this;
                axios.get('http://apis.is/petrol')
                .then(function(response){
@@ -62,5 +74,7 @@
 </script>
 
 <style>
-
+     #place:hover{
+          color: black;
+     }
 </style>
